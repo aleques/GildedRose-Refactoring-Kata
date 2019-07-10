8cb3b2012fbeced665ef898f1a2f8802cc8d65a6
Alex Oliveira <Alex.LopesdeOliveira@arhs-dev-be.com>
2019-07-10 02:56:02 +0200

    refactoring updateQualityAgedBrie reviewing business logic

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -30,15 +30,17 @@ class GildedRose {
 
     private void updateQualityAgedBrie(Item item) {
 
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-
         item.sellIn = item.sellIn - 1;
 
-        if (item.sellIn < 0 && item.quality < 50) {
-            item.quality = item.quality + 1;
+        if (item.quality < 50) {
+            if (item.sellIn < 0) {
+                item.quality = item.quality + 2;
+            } else {
+                item.quality = item.quality + 1;
+            }
         }
+
+
     }
 
     private void updateQualitySulfuras(Item item) {
