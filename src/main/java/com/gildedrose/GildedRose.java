fe599b9adaf717daca9d6427a583ef5d848d167e
Alex Oliveira <Alex.LopesdeOliveira@arhs-dev-be.com>
2019-07-10 02:49:54 +0200

    refactoring nested redundant ifs

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -48,16 +48,12 @@ class GildedRose {
         if (item.quality < 50) {
             item.quality = item.quality + 1;
 
-            if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+            if (item.sellIn < 11 && item.quality < 50) {
+                item.quality = item.quality + 1;
             }
 
-            if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+            if (item.sellIn < 6 && item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
 
