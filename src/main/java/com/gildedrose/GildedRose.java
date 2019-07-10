3df0dfd89463860a5255a320ffffa258a46f8333
Alex Oliveira <Alex.LopesdeOliveira@arhs-dev-be.com>
2019-07-10 03:27:46 +0200

    Revert "refactoring updateQualityAgedBrie reviewing business logic"
    This reverts commit efe096fe

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -30,17 +30,15 @@ class GildedRose {
 
     private void updateQualityAgedBrie(Item item) {
 
-        item.sellIn = item.sellIn - 1;
-
         if (item.quality < 50) {
-            if (item.sellIn < 0) {
-                item.quality = item.quality + 2;
-            } else {
-                item.quality = item.quality + 1;
-            }
+            item.quality = item.quality + 1;
         }
 
+        item.sellIn = item.sellIn - 1;
 
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
     }
 
     private void updateQualitySulfuras(Item item) {
